2568fff13418b76ea996bece768bc3f1722b3282
Hafez Bahrami <hafezbahrami@gmail.com>
2021-05-17 13:16:28 -0400

    initial commit

---
+++ classifier/test/test2.py
@@ -0,0 +1,12 @@
+# import torch
+# from homework.models import CNNClassifier, test_loaded_data_for_debugging, test_designed_cnn_network
+from homework.train import debug_train #, test_loss_func
+# from homework.logging import debug_logging
+from homework.viz_prediction import debug_visualization
+
+# test_loaded_data_for_debugging()
+# test_designed_cnn_network()
+# test_loss_func()
+debug_train()
+# debug_logging()
+# debug_visualization()
